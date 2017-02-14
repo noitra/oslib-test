cd5f35b4335c6b7aba37984a49672a85c317545c
Noitra <kapiau@gmail.com>
2017-02-14 14:27:06 -0200

    initial

---
+++ oslib-test/src/main/java/com/gabriel/oslib_test/OSLibTestUtils.java
@@ -0,0 +1,12 @@
+package com.gabriel.oslib_test;
+
+/**
+ * Created by Gabriel on 2/14/17.
+ */
+
+public class OSLibTestUtils {
+
+    public static String toCaps (String entry) {
+        return entry.toUpperCase();
+    }
+}
